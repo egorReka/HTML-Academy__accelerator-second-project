b83433ebf8b84b28f9dc038aeca19095dcbfaf9f
egorReka <e.reka55@yandex.ru>
2024-04-25 15:41:21 +0300

    feat: add training

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,14 @@
 import Swiper from 'swiper';
-import {Navigation, Pagination} from "swiper/modules";
+import {Navigation, Pagination} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 import { toggleNav } from './nav';
 import { initSwiperHiro } from './swiper-hero';
 import { initSwiperTours } from './tours-swiper';
+import { initSwiperTraining } from './training-swiper';
 
 toggleNav();
 initSwiperHiro(Swiper, Pagination);
 initSwiperTours(Swiper, Navigation);
+initSwiperTraining(Swiper, Navigation);
---
+++ source/js/training-swiper.js
@@ -0,0 +1,32 @@
+// TODO убрать фокус со сктытого слайдера на ссылку vk
+
+const initSwiperTraining = (Swiper, Navigation) => {
+  const swiper = new Swiper('.training-swiper', {
+    modules: [Navigation],
+    loop: false,
+    spaceBetween: 20,
+    simulateTouch: false,
+    navigation: {
+      nextEl: '.training__button--next',
+      prevEl: '.training__button--prev',
+    },
+    breakpoints: {
+      320: {
+        initialSlide: 2,
+        slidesPerView: 1,
+      },
+      768: {
+        initialSlide: 0,
+        slidesPerView: 3,
+      },
+      1366: {
+        initialSlide: 0,
+        slidesPerView: 4,
+      },
+    },
+  });
+
+  return swiper.init();
+};
+
+export {initSwiperTraining};
